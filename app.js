74d15ac5984671d507178924c70dff7223ce2700
nmandri-wipass <74370755+nmandri-wipass@users.noreply.github.com>
2023-10-08 19:10:07 +0200

    Lets push

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-// Importa la biblioteca Express
+// Importa las bibliotecas necesarias
 const express = require('express');
+const path = require('path');
 
 // Crea una instancia de la aplicación Express
 const app = express();
 
-// Define una ruta de ejemplo
+// Configura el directorio de archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define una ruta para la página principal
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Define el puerto en el que escuchará el servidor
+// Configura el puerto en el que escuchará el servidor
 const port = process.env.PORT || 3000;
 
-// Inicia el servidor y escucha en el puerto especificado
+// Inicia el servidor
 app.listen(port, () => {
-  console.log(`El servidor está funcionando en el puerto ${port}`);
+  console.log(`Servidor web funcionando en el puerto ${port}`);
 });
